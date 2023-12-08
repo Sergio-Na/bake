361faacd7b418dc909a7377fdcf5bf7368dbcd62
Sergio Najm <98653927+Sergio-Na@users.noreply.github.com>
2023-12-07 23:15:58 -0500

    added hamburger menu for small screens

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,24 +4,40 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import logo from "../../../public/cake_logo.png"
+import logo from "../../../public/cake_logo.png";
+import toggle from "../../../public/cake_hamburger_menu_toggle.svg"
+import xToggle from "../../../public/x_toggle.svg"
+import { useState } from "react";
+
 const links = [
-    { name: "Home", href: "/" },
-    { name: "About us", href: "/Men" },
-    { name: "Our Cakes", href: "/Women" },
+    { name: "About us", href: "/about-us" },
+    { name: "Our Cakes", href: "/our-cakes" },
 ];
 
 export default function Navbar() {
     const pathname = usePathname();
+    const [hamburgerToggle, setHamburgerToggle] = useState(false);
+
     return (
         <header className="mb-8 border-b bg-special">
             <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
-                <Link href="/" className="flex flex-row justify-between">
-                    <Image src={logo} alt="logo" className="h-10 w-auto" />
-                    <h1 className="text-xl md:text-4xl font-bold">
+                <div className="flex items-center">
+                    <Link href="/" className="flex items-center">
+
+                        <Image
+                            src={logo}
+                            alt="logo"
+                            className="h-24 w-auto"
+                            priority
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+
+                    </Link>
+                    <h1 className="text-xl md:text-4xl font-bold ml-3">
                         <span className="text-primary">Cakes</span>
                     </h1>
-                </Link>
+                </div>
 
                 <nav className="hidden gap-12 lg:flex 2xl:ml-16">
                     {links.map((link, idx) => (
@@ -44,15 +60,36 @@ export default function Navbar() {
                         </div>
                     ))}
                 </nav>
-
-                <div className="flex divide-x border-r sm:border-l">
-                    <Button
-                        className="flex flex-col sm:h-20 sm:w-20 md:h-24 md:w-24 bg-purple-400"
-                    >
-                        <span className="hidden text-xs font-semibold text-gray-200 sm:block">
-                            Cart
-                        </span>
-                    </Button>
+                <div className="md:hidden hover:opacity-50">
+                    <button onClick={() => setHamburgerToggle(true)}>
+                        <Image
+                            src={toggle}
+                            alt="hamburger toggle" />
+                    </button>
+                </div>
+                {/* Mobile Navigation */}
+                <div className={`${hamburgerToggle ? 'block' : 'hidden'} absolute top-0 left-0 w-full h-full bg-white md:hidden`}>
+                    {/* Close Button */}
+                    <div className="flex justify-end p-5">
+                        <button onClick={() => setHamburgerToggle(false)}>
+                            <Image
+                                src={xToggle}
+                                alt="close menu"
+                                width={24}
+                                height={24} />
+                        </button>
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                        {links.map((link, idx) => (
+                            <div key={idx} className="mb-4">
+                                <button className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary" onClick={() => setHamburgerToggle(false)}>
+                                    <a href={link.href}>
+                                        {link.name}
+                                    </a>
+                                </button>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </header>
