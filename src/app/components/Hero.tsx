0cd15420c2f279710cbd9b13a19ca42a77ad36ae
Sergio Najm <98653927+Sergio-Na@users.noreply.github.com>
2023-12-22 11:21:59 -0500

    small change to h1 call to action

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
                 <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">Amazing Cakes. Made with Love</h1>
                 <p className="text-md md:text-lg lg:text-xl mt-2 md:mt-3 lg:mt-4">Discover the Sweetness of Homemade Delights</p>
                 <button className="bg-primary text-white hover:opacity-60 font-bold py-2 px-4 rounded-full mt-4">
-                    Explore Our Cakes
+                    Explore Our Cakes!
                 </button>
             </div>
         </div>
